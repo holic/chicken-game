2061575675365a38f9d1870a35e19cefb5001ff1
Kevin Ingersoll <kingersoll@gmail.com>
2020-08-02 18:19:52 -0700

    Fill in the rest of the animations

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,18 @@ const init = async () => {
         frames: [0, 1, 0, 2, 0, 1, 0, 0],
         frameRate: 8,
       },
+      walkLeft: {
+        frames: [3, 4, 3, 5, 3, 4, 3, 3],
+        frameRate: 8,
+      },
+      walkRight: {
+        frames: [6, 7, 6, 8, 6, 7, 6, 6],
+        frameRate: 8,
+      },
+      walkUp: {
+        frames: [9, 10, 9, 11, 9, 10, 9, 9],
+        frameRate: 8,
+      },
     },
   });
 
@@ -43,14 +55,44 @@ const init = async () => {
     width: spriteSheet.frame.width * 8,
     height: spriteSheet.frame.height * 8,
     animations: spriteSheet.animations,
+    currentAnimation: spriteSheet.animations.walkDown.clone(),
   });
+
+  const player2 = kontra.Sprite({
+    x: 200,
+    y: 100,
+    width: spriteSheet.frame.width * 8,
+    height: spriteSheet.frame.height * 8,
+    animations: spriteSheet.animations,
+    currentAnimation: spriteSheet.animations.walkLeft.clone(),
+  });
+
+  const player3 = kontra.Sprite({
+    x: 300,
+    y: 100,
+    width: spriteSheet.frame.width * 8,
+    height: spriteSheet.frame.height * 8,
+    animations: spriteSheet.animations,
+    currentAnimation: spriteSheet.animations.walkRight.clone(),
+  });
+
+  const player4 = kontra.Sprite({
+    x: 400,
+    y: 100,
+    width: spriteSheet.frame.width * 8,
+    height: spriteSheet.frame.height * 8,
+    animations: spriteSheet.animations,
+    currentAnimation: spriteSheet.animations.walkUp.clone(),
+  });
+
+  const entities = [player, player2, player3, player4];
 
   const loop = kontra.GameLoop({
     update() {
-      player.update();
+      entities.forEach((entity) => entity.update());
     },
     render() {
-      player.render();
+      entities.forEach((entity) => entity.render());
     },
   });
 
