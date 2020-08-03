6a7b667f977dc15dbbfa7ea6ed466a44d11d435b
Kevin Ingersoll <kingersoll@gmail.com>
2020-08-02 17:58:15 -0700

    playing with kontra for animations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import kontra from "kontra";
+
 import Entity from "./entities/Entity";
 import Position from "./components/Position";
 import PlayerControlled from "./components/PlayerControlled";
@@ -6,25 +8,76 @@ import render from "./systems/render";
 import keyboardControl from "./systems/keyboardControl";
 import wander from "./systems/wander";
 
-const player = new Entity([new Position(30, 30), new PlayerControlled()]);
-const npc = new Entity([new Position(100, 100), new Wanderer()]);
+const CHICK_MOVE_LEGHORN = "assets/Leghorn_lv1_Move.png";
 
-console.log(player, npc);
-
-const systems = [render, keyboardControl, wander];
-
-let lastTime = performance.now();
-
-const gameLoop = () => {
-  const now = performance.now();
-  const delta = lastTime - now;
-  lastTime = now;
-
-  systems.forEach((system) => {
-    system(delta, now);
+const loadImage = (url: string) => {
+  const img = new Image();
+  return new Promise((resolve, reject) => {
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = url;
   });
-
-  requestAnimationFrame(gameLoop);
 };
 
-requestAnimationFrame(gameLoop);
+const init = async () => {
+  // TODO: loading screen?
+  const image = await loadImage(CHICK_MOVE_LEGHORN);
+
+  const spriteSheet = kontra.SpriteSheet({
+    image: image,
+    frameWidth: 11,
+    frameHeight: 11,
+    animations: {
+      walkDown: {
+        frames: [0, 1, 0, 2, 0, 1, 0, 0],
+        frameRate: 8,
+      },
+    },
+  });
+
+  const { canvas, context } = kontra.init(document.querySelector("canvas"));
+
+  const player = kontra.Sprite({
+    x: 100,
+    y: 100,
+    width: spriteSheet.frame.width * 8,
+    height: spriteSheet.frame.height * 8,
+    animations: spriteSheet.animations,
+  });
+
+  const loop = kontra.GameLoop({
+    update() {
+      player.update();
+    },
+    render() {
+      player.render();
+    },
+  });
+
+  loop.start();
+};
+
+init();
+
+// const player = new Entity([new Position(30, 30), new PlayerControlled()]);
+// const npc = new Entity([new Position(100, 100), new Wanderer()]);
+
+// console.log(player, npc);
+
+// const systems = [render, keyboardControl, wander];
+
+// let lastTime = performance.now();
+
+// const gameLoop = () => {
+//   const now = performance.now();
+//   const delta = lastTime - now;
+//   lastTime = now;
+
+//   systems.forEach((system) => {
+//     system(delta, now);
+//   });
+
+//   requestAnimationFrame(gameLoop);
+// };
+
+// requestAnimationFrame(gameLoop);
