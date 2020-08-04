16020fa2bd89eef50e8e3b79ac34ddb2e1d73b20
Kevin Ingersoll <kingersoll@gmail.com>
2020-08-03 18:49:23 -0700

    Update last player movement of NPCs

--- src/systems/wander.ts
+++ src/systems/wander.ts
@@ -1,12 +1,13 @@
 import { entitiesForComponents } from "../entities/Entity";
 import Sprite, { Facing, State } from "../components/Sprite";
 import Wanderer from "../components/Wanderer";
+import Position from "../components/Position";
 
 const rand = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 const wander = (delta: number, time: number) => {
-  const entities = entitiesForComponents([Wanderer, Sprite]);
+  const entities = entitiesForComponents([Wanderer, Sprite, Position]);
   entities.forEach((entity) => {
     if (rand(1, 100) > 5) return;
 
@@ -16,18 +17,24 @@ const wander = (delta: number, time: number) => {
       return;
     }
 
+    const position = entity.getComponent(Position);
+
     if (rand(0, 1)) {
       sprite.requestedFacing = Facing.Right;
       sprite.requestedState = State.Walking;
+      position.lastMovement = time;
     } else if (rand(0, 1)) {
       sprite.requestedFacing = Facing.Left;
       sprite.requestedState = State.Walking;
+      position.lastMovement = time;
     } else if (rand(0, 1)) {
       sprite.requestedFacing = Facing.Up;
       sprite.requestedState = State.Walking;
+      position.lastMovement = time;
     } else if (rand(0, 1)) {
       sprite.requestedFacing = Facing.Down;
       sprite.requestedState = State.Walking;
+      position.lastMovement = time;
     } else {
       sprite.requestedState = State.Idle;
     }
